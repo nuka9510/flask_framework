1f405262edb713a5f3e80291cd351f5f3c902e4d
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2022-09-16 14:57:21 +0900

    220916_1457
    system core log.py 수정

--- app.py
+++ app.py
@@ -12,15 +12,3 @@ for key in config:
     app.config[key] = config[key]
    
 app.register_blueprint(Example.bp)
-
-if not app.debug:
-    from logging import FileHandler
-
-    if not os.path.exists(log['LOG_PATH']):
-        os.makedirs(log['LOG_PATH'])
-
-    file_handler = FileHandler(filename=os.path.join(log['LOG_PATH'], log['LOG_NAME']))
-    file_handler.setFormatter(log['LOG_FORMAT'])
-
-    app.logger.setLevel(log['LOG_LEVEL'])
-    app.logger.addHandler(file_handler)
--- system/core/log.py
+++ system/core/log.py
@@ -2,9 +2,12 @@ import os, logging
 from application.config import log
 
 logger = logging.getLogger(__name__)
+
+if not os.path.exists(log['LOG_PATH']):
+    os.makedirs(log['LOG_PATH'])
+
 file_handler = logging.FileHandler(filename=os.path.join(log['LOG_PATH'], log['LOG_NAME']))
 
 file_handler.setFormatter(log['LOG_FORMAT'])
-
 logger.setLevel(log['LOG_LEVEL'])
 logger.addHandler(file_handler)
