5a28bf12a670433388e748e0bafd9ee293fe3c35
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2023-03-21 10:44:47 +0900

    230321_1044

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from system.core.converter import *
-from application.config import app as config, log
+from application.config import app as app_config, config
 from application.controllers import Example
 
 app = Flask(__name__)
 
 app.url_map.converters['regex'] = RegexConverter
 
-for key in config:
-    app.config[key] = config[key]
+for key in app_config:
+    app.config[key] = app_config[key]
    
 app.register_blueprint(Example.bp)
